e3d6b66e8e0e2e00537c15fe080ee6dc5691d705
Christopher Lyons <christopherlyons916@gmail.com>
2019-11-21 12:40:44 -0600

    Baseline code.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NewsSearcher from './NewsSearch/NewsSearcher';
+import About from './About/About';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/'>About</Link>
+          </li>
+        </ul>
+      </nav>
+
+      {/* The Switch Component looks through the Route components inside of it and renders the first one that matches the current URL */}
+      <Switch>
+        <Route exact path='/'>
+          <NewsSearcher />
+        </Route>
+        <Route path='/about'>
+          <About />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
